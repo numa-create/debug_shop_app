6c510f9367546c6a5a79c769e52849b73f2bc7f5
numa-create <k.numata0815@gmail.com>
2021-11-05 12:05:50 +0900

    バグ再修正02

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @item = Item.order(created_at: :desc)
+    @items = Item.order(created_at: :desc)
   end
 
   def new
@@ -8,7 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    Item.create!(item_params)
     redirect_to items_path
   end
 
